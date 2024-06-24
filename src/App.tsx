808174d4c9aaa272c2dbad2042dd7ac76689a3dd
minjilee <budlalswl1129@gmail.com>
2024-06-24 16:29:52 +0900

    chore: react-query, index.css, router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 function App() {
-  return <h1 className='font-hanaBold text-red-300'>하나은행</h1>;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import { Home } from './pages/main/home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
