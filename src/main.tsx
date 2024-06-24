75397045c47b15a93c3cb20ca97019dbf26bc7cd
minjilee <budlalswl1129@gmail.com>
2024-06-24 16:31:54 +0900

    chore: 파일 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// import { Home } from './pages/main/home.tsx';
+import { Home } from './pages/main/Home.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    // children: [{ index: true, element: <Home /> }],
+    children: [{ index: true, element: <Home /> }],
   },
 ]);
 
