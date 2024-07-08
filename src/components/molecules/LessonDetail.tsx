344035da2f6ae25344a7f6a328490a95dde821dd
alswlfl29 <mjl001129@naver.com>
2024-07-08 04:07:15 +0000

    fix: 클래스 상세 정보-상세 설명 글자수 수정

--- src/components/molecules/LessonDetail.tsx
+++ src/components/molecules/LessonDetail.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 interface Iprops {
   lessonDetail: BaseResponseType<LessonDetailType> | null | undefined;
 }
 
 export const LessonDetail = ({ lessonDetail }: Iprops) => {
+  const [isDescriptionExpanded, setDescriptionExpanded] = useState(false);
+
   const category = lessonDetail?.data?.categoryName;
   const people = lessonDetail?.data?.capacity;
   const price = lessonDetail?.data?.price;
@@ -10,12 +14,16 @@ export const LessonDetail = ({ lessonDetail }: Iprops) => {
   const materials = lessonDetail?.data?.materials;
   const description = lessonDetail?.data?.description;
 
+  const toggleDescription = () => {
+    setDescriptionExpanded(!isDescriptionExpanded);
+  };
+
   const formatNumber = (value: number) => {
     return new Intl.NumberFormat('ko-KR').format(value);
   };
 
   return (
-    <div className='mt-3 mb-32 px-5 py-3 w-[351px] h-[122px] font-hanaRegular text-xs bg-white rounded-2xl border-[1px] border-hanaSilver'>
+    <div className='mt-3 mb-32 px-5 py-3 w-[351px] font-hanaRegular text-xs bg-white rounded-2xl border-[1px] border-hanaSilver'>
       <div className='flex flex-row'>
         {lessonDetail?.data?.image ? (
           <img
@@ -33,7 +41,20 @@ export const LessonDetail = ({ lessonDetail }: Iprops) => {
           <p>가격 : {formatNumber(Number(price) || 0)} 원</p>
           <p>장소 : {place}</p>
           <p>준비물 : {materials} </p>
-          <p>상세설명 : {description}</p>
+          <p>
+            상세설명 :{' '}
+            {description && description.length > 20 && !isDescriptionExpanded
+              ? `${description.substring(0, 15)} ...`
+              : description}
+            {description && description.length > 20 && (
+              <button
+                onClick={toggleDescription}
+                className='text-hanaSilver ml-1'
+              >
+                {isDescriptionExpanded ? '접기' : '더보기'}
+              </button>
+            )}
+          </p>
         </div>
       </div>
     </div>
